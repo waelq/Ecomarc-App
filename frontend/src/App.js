b654454361c16b4d87e60633638ad98070871da5
unknown <waealtaqia20152729@gmail.com>
2020-11-15 23:36:15 +0200

    Add Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { Container } from "react-bootstrap";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import HomeScreen from "./Screens/HomeScreen";
+import Productscreen from "./Screens/Productscreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
           {" "}
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={Productscreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
